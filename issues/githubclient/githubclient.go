59d8a2ab78719d6ffe5d24baca2097a504283902
sanwakuwaku <sanwaku2@gmail.com>
2019-12-09 16:14:00 +0900

    github issue api用の構造体にメンバ追加

--- issues/githubclient/githubclient.go
+++ issues/githubclient/githubclient.go
@@ -11,19 +11,36 @@ const (
 	IssuesURL       = RootEndpoint + "/repos/" + RepoOwner + "/" + RepoName + "/issues"
 )
 
+type IssuesSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
 type Issue struct {
 	Number    int
 	HTMLURL   string `json:"html_url"`
 	Title     string
 	State     string
 	User      *User
-	CreatedAt time.Time `json:"created_at"`
-	Body      string    // in Markdown format
+	Milestone *Milestone `json:"milestone,omitempty"`
+	CreatedAt time.Time  `json:"created_at"`
+	Body      string     // in Markdown format
 }
 
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+type Milestone struct {
+	Number       int
+	HTMLURL      string `json:"html_url"`
+	State        string
+	Title        string
+	Desctiption  string
+	OpenIssues   int       `json:"open_issues"`
+	ClosedIssues int       `json:"closed_issues"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 type IssueRequestParams struct {
